9c918593333dd459336481f934d1485674f5d93f
Moises Santos <moisesbarbosa23@gmail.com>
2024-02-10 12:45:31 -0300

    feat(filterHttp): removendo try catch e adicionando filter para tratamento de exceções.

---
+++ src/filtra-todas-excecoes/filtra-todas-excecoes.filter.spec.ts
@@ -0,0 +1,7 @@
+import { FiltraTodasExcecoesFilter } from './filtra-todas-excecoes.filter';
+
+describe('FiltraTodasExcecoesFilter', () => {
+  it('should be defined', () => {
+    expect(new FiltraTodasExcecoesFilter()).toBeDefined();
+  });
+});
---
+++ src/filtra-todas-excecoes/filtra-todas-excecoes.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class FiltraTodasExcecoesFilter<T> implements ExceptionFilter {
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error:
+        exception instanceof HttpException
+          ? exception.getResponse()
+          : 'Erro interno.',
+    });
+  }
+}
