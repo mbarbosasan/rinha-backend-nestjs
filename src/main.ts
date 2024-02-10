14605b2d9aa4c5e811592dd29ed192e6b1117330
Moises Santos <moisesbarbosa23@gmail.com>
2024-02-10 12:56:10 -0300

    feat(filterHttp): registro do filtro no main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FiltraTodasExcecoesFilter } from './filtra-todas-excecoes/filtra-todas-excecoes.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new FiltraTodasExcecoesFilter());
   await app.listen(3000);
 }
 
